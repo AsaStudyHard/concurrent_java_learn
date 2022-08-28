1a0dadc489d16fc7c65975d8ad3a36e2ad4706d9
ZhuJunfei <zhujunfeisnow@163.com>
2022-08-28 14:49:19 +0800

    创建多线程的2种方式

---
+++ src/main/java/com/zjf/chapter01thread/StartThreadMethod.java
@@ -0,0 +1,39 @@
+/**
+ * description:  创建线程的方式
+ * date:         2022/8/28 12:31
+ * author        ZhuJunfei
+ */
+
+package com.zjf.chapter01thread;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.StartThreadMethod")
+public class StartThreadMethod {
+    public static void testThread() {
+        Thread t1 = new Thread("t1") {
+            @Override
+            public void run() {
+                log.debug("extends thread running");
+            }
+        };
+        t1.start();
+    }
+
+    public static void testRunnable() {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                log.debug("implement runnable running");
+            }
+        };
+
+        Thread t2 = new Thread(task, "t2");
+        t2.start();
+    }
+
+    public static void main(String[] args) {
+        testRunnable();
+        testThread();
+    }
+}
