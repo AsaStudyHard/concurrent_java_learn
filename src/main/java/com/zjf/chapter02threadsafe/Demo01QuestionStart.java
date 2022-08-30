70a04f17205a63939e6e78b1618ccdf4f67af5c7
ZhuJunfei <zhujunfeisnow@163.com>
2022-08-30 14:09:45 +0800

    使用OOP修改我们的共享问题

--- src/main/java/com/zjf/chapter02threadsafe/Demo01QuestionStart.java
+++ src/main/java/com/zjf/chapter02threadsafe/Demo01QuestionStart.java
@@ -11,15 +11,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "c.Demo01QuestionStart")
 public class Demo01QuestionStart {
     static int counter = 0;
+    static Object lock = new Object();
+
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                counter++;
+                synchronized (lock){
+                    counter++;
+                }
             }
         }, "t1");
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                counter--;
+                synchronized (lock){
+                    counter--;
+                }
             }
         }, "t2");
         t1.start();
